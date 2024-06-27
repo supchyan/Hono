37ccdb1720a6d982396f5e92714cfd473c3f135a
whale-linux <mikhailsupov@yandex.ru>
2024-06-27 11:03:04 +0300

    g

--- src/buttons/videoBtn.js
+++ src/buttons/videoBtn.js
@@ -2,4 +2,5 @@ const videoContainer = document.getElementById('video-container');
 const videoBtn = document.getElementById('video-btn')
 videoBtn.onclick = (() => {
     videoContainer.style.visibility = videoContainer.style.visibility == 'visible' ? 'hidden' : 'visible'
+    videoContainer.style.zIndex = videoContainer.style.visibility == 'visible' ? '1' : '-1'
 })
