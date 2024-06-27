2ee4efb2d1fbdcb70d40b43607e209a75f71a600
whale-linux <mikhailsupov@yandex.ru>
2024-06-27 10:59:16 +0300

    etheryert

--- src/buttons/videoBtn.js
+++ src/buttons/videoBtn.js
@@ -1,6 +1,5 @@
 const videoContainer = document.getElementById('video-container');
 const videoBtn = document.getElementById('video-btn')
 videoBtn.onclick = (() => {
-    console.log(videoContainer.checkVisibility)
     videoContainer.style.visibility = videoContainer.style.visibility == 'visible' ? 'hidden' : 'visible'
 })
